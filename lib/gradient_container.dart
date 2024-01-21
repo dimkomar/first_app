06a302c28f0a465c35d1022f89fb1167b11c60bb
Dmitriy Komar <dimkomar@gmail.com>
2024-01-21 17:14:40 +0100

    c1

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,23 +1,49 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
+
+var topAlignment = Alignment.topLeft;
+var bottomAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 127, 58, 215),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: colors,
+            begin: topAlignment,
+            end: bottomAlignment,
+          ),
         ),
-      ),
-      child: const StyledText(),
+        child: const Center(
+          child: DiceRoller()
+        ),
     );
   }
 }
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+//
+//   final List<Color> colors;
+//
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: topAlignment,
+//           end: bottomAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//          child: StyledText('Hello World'),
+//       )
+//     );
+//   }
+// }
--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -2,18 +2,18 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+  const StyledText(this.text, {super.key});
+
+  final String text;
 
   @override
   Widget build( context) {
-    return const Center(
-      child: Text(
-        'Hello World1',
-        style: TextStyle(
+      return Text(
+        text,
+        style: const TextStyle(
           color: Colors.white,
           fontSize: 28,
         ),
-      ),
     );
   }
 }
